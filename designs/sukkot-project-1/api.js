23989f8966c102ac4eb3d339ca3aaafa0f13d5aa
nimrod amos <nimrodamos100@gmail.com>
2024-10-24 23:46:59 +0300

    new

--- designs/sukkot-project-1/api.js
+++ designs/sukkot-project-1/api.js
@@ -1,3 +1,5 @@
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
 // Function to fetch popular movies from the API
 export async function fetchPopularMovies() {
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
